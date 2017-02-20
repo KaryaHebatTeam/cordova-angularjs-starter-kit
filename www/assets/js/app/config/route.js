26782b7a1e25baac4fe21ff3d8f2ceff080bc66c
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2017-02-20 15:16:13 +0700

    Splash added

--- www/assets/js/app/config/route.js
+++ www/assets/js/app/config/route.js
@@ -2,9 +2,14 @@ angular
 	.module('Starter')
 	.config(Route);
 
-function Route($routeProvider) {
+function Route($routeProvider, $locationProvider) {
+	$locationProvider.hashPrefix('');
+
 	$routeProvider
 		.when('/', {
+			templateUrl: 'view/splash.html'
+		})
+		.when('/friend-list', {
 			templateUrl: 'view/friend-list.html'
 		});
 }
